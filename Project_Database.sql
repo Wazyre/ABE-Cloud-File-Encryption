1765057f406e0a43b20e2dd8f46d3a8f82aa5fa7
Wazyre <32106559+Wazyre@users.noreply.github.com>
2024-03-17 22:48:10 -0700

    Update SQL

--- Project_Database.sql
+++ Project_Database.sql
@@ -7,15 +7,16 @@ SET character_set_client = utf8mb4 ;
 
 CREATE TABLE `aws_user` (
   `user_id` int(11) NOT NULL,
-  `name` varchar(50) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
   `email` varchar(50) NOT NULL,
   `year` varchar(2) NOT NULL,
   `department` varchar(50) NOT NULL,
   `position` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-INSERT INTO `aws_user` VALUES (1,'Alice','aliced@uoregon.edu','10','Physics','Professor');
-INSERT INTO `aws_user` VALUES (2,'Bob','bobs2@gmail.com','1','Computer Science','Teaching Assistant');
-INSERT INTO `aws_user` VALUES (3,'Candice','candicel@hotline.com','4','Business','Graduate Student');
-INSERT INTO `aws_user` VALUES (4,'Donald','donaldt@osu.edu','3','Human Phys','Undergraduate Student');
-INSERT INTO `aws_user` VALUES (5,'Eve','evef@uoregon.edu','5','Journalism','Professor');
+INSERT INTO `aws_user` VALUES (1,'Alice','cn029!38r4yc09@@h','aliced@uoregon.edu','10','Physics','Professor');
+INSERT INTO `aws_user` VALUES (2,'Bob','nv203#98vr#V79y29v8','bobs2@gmail.com','1','Computer Science','Teaching Assistant');
+INSERT INTO `aws_user` VALUES (3,'Candice','#*V&v38V*&v0#&^VB','candicel@hotline.com','4','Business','Graduate Student');
+INSERT INTO `aws_user` VALUES (4,'Donald','CN##@#C23cn#C*37c3','donaldt@osu.edu','3','Human Phys','Undergraduate Student');
+INSERT INTO `aws_user` VALUES (5,'Eve','H&^AHS&A^868as8dS','evef@uoregon.edu','5','Journalism','Professor');
